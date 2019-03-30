8ce209525cfa0e603b2e1155667c687edcbedcf0
孙磊 <382629233@qq.com>
2019-03-30 13:31:59 +0800

    完成day09 修改brandname categoryname错误 和index type错误

--- gmall-api/src/main/java/com/atguigu/gmall/to/es/SearchParam.java
+++ gmall-api/src/main/java/com/atguigu/gmall/to/es/SearchParam.java
@@ -10,8 +10,8 @@ import java.io.Serializable;
 @Data
 public class SearchParam implements Serializable{
 
-    private Long[] catelog3Id;//三级分类ID
-    private Long[] brandId;//品牌ID
+    private String[] catelog3;//三级分类
+    private String[] brand;//品牌
     private String keyword;//检索关键字
     private String order;//0:综合,1:销量，2：价格，3价格区间
     private Integer pageNum=1;//当前页
