0fa77acf238cf7b30be7c1aa401b2e827738c416
孙磊 <382629233@qq.com>
2019-03-30 08:39:50 +0800

    完成day09 lock test 完成

--- redis-test/src/main/java/com/atguigu/locks/controller/RedisController.java
+++ redis-test/src/main/java/com/atguigu/locks/controller/RedisController.java
@@ -10,19 +10,61 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController {
     @Autowired
     RedisService redisService;
+
     @GetMapping("/incr")
-    public String redisIncr(){
+    public String redisIncr() {
         redisService.incr();
         return "ok";
     }
+
     @GetMapping("/lock")
-    public String lock(){
+    public String lock() {
         redisService.getLockUnlock();
         return "ok";
     }
+
     @GetMapping("/getlock/{ops}")
-    public String getLock(@PathVariable("ops")String ops){
+    public String getLock(@PathVariable("ops") String ops) {
         redisService.getLock(ops);
         return "ok";
+    }
+
+    /**
+     * 读写的目的，锁细化
+     *
+     * @return
+     */
+    @GetMapping("/read")
+    public String readValue() {
+        return redisService.readValue();
+    }
+
+    @GetMapping("/write")
+    public String writeValue() {
+        return redisService.writeValue();
+    }
+
+    @GetMapping("/park")//停车
+    public String park() {
+        return redisService.park();
+    }
+
+    @GetMapping("/out")
+    public String out() {
+
+        return redisService.out();
+    }
+
+    @GetMapping("/lockdoor")
+    public String lockdoor(){
+        System.out.println("我想要锁门");
+        redisService.lockdoor();
+        return "班长锁门了";
+    }
+    @GetMapping("/go")
+    public String go(){
+        System.out.println("溜了");
+        redisService.go();
+        return "溜了";
     }
 }
--- redis-test/src/main/java/com/atguigu/locks/service/RedisService.java
+++ redis-test/src/main/java/com/atguigu/locks/service/RedisService.java
@@ -1,11 +1,13 @@
 package com.atguigu.locks.service;
 
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.*;
 import org.redisson.client.RedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class RedisService {
@@ -39,5 +41,68 @@ public class RedisService {
             lock.lock();
             System.out.println("lock()获取到锁");
         }
+    }
+
+    public String readValue() {
+        RReadWriteLock readWriteLock = redissonClient.getReadWriteLock("hello-lock");
+        RLock rLock = readWriteLock.readLock();
+        rLock.lock();
+        String hello = redisTemplate.opsForValue().get("hello");
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        rLock.unlock();
+        return hello;
+    }
+
+    public String writeValue() {
+        RReadWriteLock readWriteLock = redissonClient.getReadWriteLock("hello-lock");
+        RLock writeLock = readWriteLock.writeLock();
+        writeLock.lock();
+        redisTemplate.opsForValue().set("hello", UUID.randomUUID().toString());
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        writeLock.unlock();
+        return "ok";
+    }
+
+    public String park() {
+        RSemaphore semaphore = redissonClient.getSemaphore("parklength");
+        try {
+            semaphore.acquire();
+            System.out.println("找到车位了");
+            return "ok";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "fail";
+    }
+
+    public String out() {
+        RSemaphore semaphore = redissonClient.getSemaphore("parklength");
+        semaphore.release();
+        return "释放车位完成";
+    }
+
+    public void lockdoor() {
+        RCountDownLatch latch = redissonClient.getCountDownLatch("students");
+
+        //等待别人都走完
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("锁门了。。。bye");
+    }
+
+    public void go() {
+        RCountDownLatch latch = redissonClient.getCountDownLatch("students");
+        latch.countDown();
     }
 }
